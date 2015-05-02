6e6fd6612fe68d9a502514d8f31fed95848c0307
Henrry Salinas <es.henrry@gmail.com>
2015-05-02 12:20:20 -0400

    new changes in dominio layer

--- DominioServicios.SisReservas/Interfaz/IDominioReportes.cs
+++ DominioServicios.SisReservas/Interfaz/IDominioReportes.cs
@@ -9,7 +9,7 @@ namespace DominioServicios.SisReservas.Interfaz
     public interface IDominioReportes
     {
         
-
+        //Interfaces de la capa de dominio
         List<O_LISTAR_GENERO> ListarGenero(string strCredencial, O_LISTAR_GENERO oListarGenero, ref string strMensajeError);
         List<O_LISTAR_PERSONA> ListarPersona(string strCredencial, O_LISTAR_PERSONA oListarPersona, ref string strMensajeError);
         List<O_LISTAR_DOCUMENTO> ListarDocumento(string strCredencial, O_LISTAR_DOCUMENTO oListarDocumento, ref string strMensajeError);
